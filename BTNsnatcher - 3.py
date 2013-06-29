339ec1c5f89215986c06ad1e5ee8893a1735d875
detemps <detemps@detemps.de>
2013-06-29 22:55:17 +0300

    BP estimation for PY3 version

--- BTNsnatcher - 3.py
+++ BTNsnatcher - 3.py
@@ -15,35 +15,56 @@ class User(object):
 		self.torrentList = tlist
 		self.session = requests.session()
 		self._login()
-		time.sleep(2)
 		### THIS MIGHT NEED TO CHANGE
 		self.userID = self._getUserID()
 		print("Successfully logged in to the site!\n")
 		### THIS MIGHT NEED TO CHANGE
-		self.snatchedNumber = 0
-		self.needed = 0
-		self.spaceNeeded = 0
+		self._getUserDetails()
 
 	def _login(self):
 		loginURL = "https://broadcasthe.net/login.php"
 		self.page = self.session.post(loginURL, data = self.loginDetails, headers = self.headers)
+		time.sleep(2)
 
 	def _getUserID(self):
 		self.page = self.session.get("https://broadcasthe.net/index.php").text
 		soup = bs4.BeautifulSoup(self.page)
 		info = soup.find(id = "userinfo_username")
 		userID = info.li.a.get("href").split("=")[1]
+		time.sleep(2)
 		return userID
 
-	def _getSnatchCount(self):
+	def _getUserDetails(self):
 		profileURL = "https://broadcasthe.net/user.php?id=%s" % self.userID
 		self.page = self.session.get(profileURL).text
 		soup = bs4.BeautifulSoup(self.page).find(id = "section2").div
 		statistics = soup.find_all("div")[1]
 		upDownStats = statistics.find_all("div")[2].ul
 		uploadsSnatched = int(upDownStats.find_all("li")[2].string.split(":")[1].strip())
+		seed = upDownStats.find_all("li")[4].string.split(":")[1].strip()
 		downSnatched = int(upDownStats.find_all("li")[7].contents[0][:-1].split(":")[1].strip())
-		return uploadsSnatched + downSnatched
+		snatched = uploadsSnatched + downSnatched
+		seedNo = float(seed.split()[0].replace(",", ""))
+		seedStr = seed.split()[1]
+		if seedStr.lower() == "mb":
+			seedSize = seedNo / 1024.0
+		elif seedStr.lower() == "gb":
+			seedSize = seedNo
+		elif seedStr.lower() == "tb":
+			seedSize = seedNo * 1024.0
+		else:
+			seedSize = 0
+		first = statistics.find_all("div")[0].ul
+		userClass = first.find_all("li")[1].string.split(":")[1].strip()
+		bonusPoints = float(first.find_all("a")[0].string.replace(",", ""))
+		bonusRate = float(first.find_all("li")[5].string.split(":")[1].strip().replace(",", ""))
+		self.snatchedNumber = snatched
+		self.seedSize = seedSize
+		self.userClass = userClass
+		self.bonusPoints = bonusPoints
+		self.needed = 3000 - snatched
+		self.bonusRate = float(bonusRate)
+		time.sleep(2)
 
 	def _getSnatchList(self):
 		start = 1
@@ -72,6 +93,7 @@ class User(object):
 		for option in options:
 			if "selected" in str(option):
 				return int(option.string)
+		time.sleep(2)
 
 	def _addToTorrentList(self, torrentTable):
 		add = 0
@@ -110,7 +132,7 @@ class User(object):
 			start += 1
 			time.sleep(2)
 
-	def downloadTorrents(self):
+	def _downloadTorrents(self):
 		downloadURL = "https://broadcasthe.net/torrents.php?action=download&id=%s"
 		for name, torrent in enumerate(self.torrentList):
 			filename = "BTN%s" % name + ".torrent"
@@ -138,13 +160,77 @@ class User(object):
 			extra = 0
 		return extra
 
+	def bonus(self):
+		userClasses = ("user", "member", "power user", "extreme user", "elite", "guru", "master")
+		bonusReq = (0, 100000, 250000, 500000, 850000, 1500000, 3000000)
+		currentClass = userClasses.index(self.userClass.lower())
+		nextClass = currentClass + 1
+		bonusNeed = bonusReq[nextClass] - self.bonusPoints
+		for i in bonusReq[nextClass + 1:]:
+			bonusNeed += i
+		if bonusNeed <= 0:
+			print("You already have enough points to be promoted to Master user class.")
+			return
+		print("You need %s bonus points to get to the Master user class." % bonusNeed)
 
-	def setup(self):
+		self.page = self.session.get("https://broadcasthe.net/bonus.php?action=rate").text
+		soup = bs4.BeautifulSoup(self.page)
+		seedTable = soup.find(id = "myTable").find_all("tr")[1:]
+		episodeSize = 0
+		episodePoint = 0
+		seasonSize = self.seedSize
+		for seed in seedTable:
+			torrent = seed.find_all("td")
+			if torrent[1].string == "Episode":
+				size, unit = torrent[2].string.split()
+				if unit.lower() == "mb":
+					a = float(size.replace(",", "")) / 1024.0
+				else:
+					a = float(size.replace(",", ""))
+				seasonSize -= float(a)
+				episodeSize += float(a)
+				episodePoint += float(torrent[-1].string.replace(",", ""))
+			else:
+				continue
+
+		print("You are earning %s per month from episodes" % episodePoint)
+		print("You are earning %s per month from season packs." % (self.bonusRate - episodePoint))
+
+		print(seasonSize)
+
+		a = 1500 * .15 * seasonSize
+		b = a + self.bonusRate
+		c = -bonusNeed
+		x = (-b + ((b**2 - (4 * a * c)) ** 0.5)) / (2 * a)
+
+		print("At your current seeding size, you need %0.1f months (around %0.1f days) to get enough points for Master" % (x, x * 30))
+		print()
+		print("If you have a number of months at the end of which you wish to have enough points please enter that number.")
+		while True:
+			try:
+				months = float(input("Number of months at the end of which you will have enough points for Master: "))
+				if months >= x:
+					print("You are already seeding enough to accumulate points for Master user class in %s months." % months)
+				else:
+					d = bonusNeed - (months * self.bonusRate) - ((months * months + months) * 0.15 * 1500 * seasonSize)
+					e = (0.15 * 3000 * months + ((months * months + months) * 0.15 * 1500))
+					new = d / e
+					print("You will need to seed %0.1f GB more in SEASON packs in order to get enough points for Master user class in %s months. \n" % (new, months))
+				ch = input("Would you like to try with another number of months? (y/n)")
+				if ch.lower() != "y":
+					break
+			except:
+				print("You haven't entered a number.\n")
+
+	def snatch(self):
 		time.sleep(1)
-		self.snatchedNumber = self._getSnatchCount()
-		self.needed = 3000 - self.snatchedNumber
+		input("Press return to begin the initialization process. ")
+		if self.needed <= 0:
+			print("You have already snatched enough torrents for the Master user class requirements.")
+			return
+		print("*" * 50)
 		print(">>> You have snatched %s torrent(s) to date." % self.snatchedNumber)
-		print(">>> You need to snatch %s torrent(s) in order to meet the Master class requirement." % self.needed)
+		print(">>> You need to snatch %s torrent(s) in order to meet the Master user class requirement." % self.needed)
 		print() 
 		print(">>> Now populating your snatch list so that we don't download torrents you have already snatched.")
 		print(">>> This may take a while depending on the size of your snatch list.")
@@ -165,22 +251,6 @@ class User(object):
 		self._getTorrentIDs()
 		print(">>> %s torrent(s) have been added to your to-download list." % len(self.torrentList))
 		print()
-
-
-def main():
-	print("Welcome to BTNsnatcher") 
-	print("*" * 50)
-	username = input("BTN username: ")
-	password = input("BTN password: ")
-	print()
-	try:
-		user = User(username, password)
-		print("Your user ID: %s" % user.userID)
-		time.sleep(2)
-		input("Press return to begin the initialization process. ")
-		print("*" * 50)
-		user.setup()
-		time.sleep(2)
 		input("Press return to begin downloading the torrent files. ")
 		try:
 			os.mkdir("BTNsnatcher_torrents")
@@ -195,14 +265,50 @@ def main():
 			else:
 				print("Please delete the 'BTNsnatcher_torrents' folder from the working directory and rerun the script.")
 		os.chdir("BTNsnatcher_torrents")
-		user.downloadTorrents()
+		self._downloadTorrents()
 		print("*" * 50)
 		print()
-		print("Finished downloading %s torrents from BTN. You will need a total space of %0.3f GBs to download all of these torrents." % (len(user.torrentList), user.spaceNeeded))
+		print("Finished downloading %s torrents from BTN. You will need a total space of %0.3f GBs to download all of these torrents." % (len(self.torrentList), self.spaceNeeded))
 		print()
 		print("*" * 50)
 		print("All done! Have a nice day!")
-	except AttributeError:
+
+
+def main():
+	print("Welcome to BTNsnatcher") 
+	print("*" * 50)
+	username = input("BTN username: ")
+	password = input("BTN password: ")
+	print()
+	try:
+		user = User(username, password)
+		print("Your user ID: %s" % user.userID)
+		print()
+		print("What would you like to do?")
+		print("Press 1 for time estimation to accumulate enough points for promotion to Master user class.")
+		print("Press 2 to snatch torrents.")
+		while True:
+			ch = input("Your choice? ")
+			if ch == "1":
+				print()
+				print("*" * 50)
+				user.bonus()
+				print("*" * 50)
+				print()
+				ch2 = input("Would you like to snatch torrents as well? (y/n) ")
+				if ch2.lower() == "y":
+					user.snatch()
+				else:
+					print("All done! Have a nice day!")
+				break
+			elif ch == "2":
+				user.snatch()
+				break
+			else:
+				print("That is not a valid choice, please choose 1 for the bonus point program")
+				print("or choose 2 to snatch torrents.")
+	except AttributeError as err:
+		print(err)
 		print("Something has gone wrong.")
 		print("I'm saving the last page I visited in the same directory I'm running in.")
 		f = open("lastPage.html", "a+")
